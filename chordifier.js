bc208494c697375bd76d17ed70dba05d8046c4ad
Peter Metzger <metzger.peter@gmail.com>
2024-03-26 17:27:08 -0400

    pseudocode version of this part

---
+++ chordifier.js
@@ -0,0 +1,17 @@
+//This file will be both a library of progressions and a hub for processing progressions into desired chords based on the key provided.
+
+//Structure of progressions:
+//mood, [chord1], [chord2], [chord3], [chord4]
+
+//Structure of chords in progressions
+//root note 1-7
+//triad type 1 major, 2 minor, 3 augmented, 4 diminished
+//7th type 0 octave, 1 major, 2 minor, 3 diminished
+
+//Processing
+//receive mood, root note
+//locate matching progressions, process and output
+
+//Output structure:
+//[chordname, note1, note2, note3, note4]
+//[...]
