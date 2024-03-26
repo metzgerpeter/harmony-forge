7abd3da841bac2de8598517444c1d4a0d3778fa5
Peter Metzger <metzger.peter@gmail.com>
2024-03-26 17:43:24 -0400

    added more details and examples

--- chordifier.js
+++ chordifier.js
@@ -1,7 +1,9 @@
 //This file will be both a library of progressions and a hub for processing progressions into desired chords based on the key provided.
 
 //Structure of progressions:
-//mood, [chord1], [chord2], [chord3], [chord4]
+//ID, [chord1], [chord2], [chord3], [chord4]
+
+//mood library will just be a list of ID's that match
 
 //Structure of chords in progressions
 //root note 1-7
@@ -10,8 +12,25 @@
 
 //Processing
 //receive mood, root note
-//locate matching progressions, process and output
+//locate matching progressions (using mood library), process and output
 
-//Output structure:
+//Output structure per prepared progression:
 //[chordname, note1, note2, note3, note4]
 //[...]
+
+//Example: if the progression is "I, vi, IV, V7" and the root note is C, chords would be stored as:
+	//[
+		//ID number,
+		//[1,1,0],
+		//[6,2,0],
+		//[4,1,0],
+		//[5,1,2]
+	//]
+
+//and it would output:
+	//[
+	//	[C, 60, 64, 67, 72],
+	//	[Am, 69, 72, 76, 81],
+	//	[F, 65, 69, 72, 77],
+	//	[G7, 67, 71, 74, 77]
+	//]
