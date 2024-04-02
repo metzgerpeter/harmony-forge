3ce12cf688dc231506acf38a7bf20a4610a8d2fe
Peter Metzger <metzger.peter@gmail.com>
2024-04-02 18:02:11 -0400

    added remaining moods to library

--- chordifier.js
+++ chordifier.js
@@ -80,6 +80,105 @@ const progSource3 = new progressionSource(
 	]
 );
 
+const progSource4 = new progressionSource(
+	4,				//ID
+	["dark"],		//mood(s)
+	[				//array of chord recipes
+		[6,2,0],		//chord1: vi
+		[4,1,1],		//chord2: IV maj7
+		[4,2,0],		//chord3: iv
+		[1,1,0]			//chord4: I
+	]
+);
+
+const progSource5 = new progressionSource(
+	5,				//ID
+	["dark"],		//mood(s)
+	[				//array of chord recipes
+		[6,2,0],		//chord1: vi
+		[5,1,0],		//chord2: V
+		[2,2,0],		//chord3: ii
+		[3,2,0]			//chord4: iii
+	]
+);
+
+const progSource6 = new progressionSource(
+	6,				//ID
+	["dark"],		//mood(s)
+	[				//array of chord recipes
+		[6,2,0],		//chord1: vi
+		[1,1,0],		//chord2: I
+		[5,1,0],		//chord3: V
+		[2,2,0]			//chord4: ii
+	]
+);
+
+const progSource7 = new progressionSource(
+	7,				//ID
+	["joyous"],		//mood(s)
+	[				//array of chord recipes
+		[1,1,0],		//chord1: I
+		[4,1,0],		//chord2: IV
+		[2,2,0],		//chord3: ii
+		[5,1,0]			//chord4: V
+	]
+);
+
+const progSource8 = new progressionSource(
+	8,				//ID
+	["joyous"],		//mood(s)
+	[				//array of chord recipes
+		[2,2,0],		//chord1: ii
+		[1,1,0],		//chord2: I
+		[5,1,0],		//chord3: V
+		[5,1,0]			//chord4: V
+	]
+);
+
+const progSource9 = new progressionSource(
+	9,				//ID
+	["joyous"],		//mood(s)
+	[				//array of chord recipes
+		[1,1,0],		//chord1: I
+		[4,1,0],		//chord2: IV
+		[5,1,0],		//chord3: V
+		[4,1,0]			//chord4: IV
+	]
+);
+
+const progSource10 = new progressionSource(
+	10,				//ID
+	["reflective"],	//mood(s)
+	[				//array of chord recipes
+		[1,1,0],		//chord1: I
+		[6,2,0],		//chord2: vi
+		[4,1,1],		//chord3: IV maj7
+		[5,1,0]			//chord4: V
+	]
+);
+
+const progSource11 = new progressionSource(
+	11,				//ID
+	["reflective"],	//mood(s)
+	[				//array of chord recipes
+		[1,1,0],		//chord1: I
+		[5,1,0],		//chord2: V
+		[6,2,0],		//chord3: vi
+		[4,1,1]			//chord4: IV maj7
+	]
+);
+
+const progSource12 = new progressionSource(
+	12,				//ID
+	["reflective"],	//mood(s)
+	[				//array of chord recipes
+		[1,1,0],		//chord1: I
+		[4,1,1],		//chord2: IV maj7
+		[3,2,0],		//chord3: iii
+		[5,1,0]			//chord4: V
+	]
+);
+
 class Chord {
 	constructor(name, notes) {
 		this.name = name;
